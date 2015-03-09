65061ace6d9fd335a0ea28c6e7b0cd5d4b8b5bd7
Tiago <tiagocis@gmail.com>
2015-03-09 13:36:55 -0300

    Bump to test CI hook

--- spec/models/my_spec.rb
+++ spec/models/my_spec.rb
@@ -4,4 +4,8 @@ describe 'My Fantastic Model' do
   it 'should always return true =D' do
     expect(true).to be_truthy
   end
+
+  it 'should always return true (part 2) :)' do
+    expect(true).to be_truthy
+  end
 end
